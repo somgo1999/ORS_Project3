ab1cad14562a69d4b4edb9dd9f2112767ecf57ae
somgo1999 <som.go1999@gmail.com>
2023-09-11 13:44:00 +0530

    ORS_Project3

---
+++ project_3/src/main/java/in/co/rays/project_3/dto/DropdownList.java
@@ -0,0 +1,14 @@
+package in.co.rays.project_3.dto;
+
+/**
+ * DropdownList interface is implemented by Beans those are used to create drop
+ * down list on HTML pages
+ * 
+ * @author Rahul Goswami
+ *
+ */
+public interface DropdownList {
+	public String getKey();
+
+	public String getValue();
+}

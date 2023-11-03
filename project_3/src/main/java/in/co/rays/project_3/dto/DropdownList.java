da7a566c73a4f55e0a711c4a435105a94a15c00e
Rahul Goswami <112253742+somgo1999@users.noreply.github.com>
2023-11-03 17:24:22 +0530

    ORS_Project3

--- project_3/src/main/java/in/co/rays/project_3/dto/DropdownList.java
+++ project_3/src/main/java/in/co/rays/project_3/dto/DropdownList.java
@@ -1,7 +1,7 @@
 package in.co.rays.project_3.dto;
 
 /**
- * DropdownList interface is implemented by Beans those are used to create drop
+ * DropdownList interface is implemented by DTOs those are used to create drop
  * down list on HTML pages
  * 
  * @author Rahul Goswami
